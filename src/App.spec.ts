74d3e6fb4d6dc4959efcbe8038a2931137d5ccc0
muzafarova <alisapsee@gmail.com>
2025-08-12 19:37:59 +0100

    remove boilerplate from App.vue

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -4,8 +4,8 @@ import { mount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('mounts and renders properly', () => {
     const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+    expect(wrapper.text()).toContain('Hospital Inventory')
   })
 })
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Hospital Inventory</h1>
+  <router-view />
 </template>
 
 <style scoped></style>
