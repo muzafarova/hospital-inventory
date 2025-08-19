8a754789e87a1b48a2b8d9c2df2905905d473b3b
muzafarova <alisapsee@gmail.com>
2025-08-19 21:48:52 +0100

    note vueDevTools issue in comment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools' // compatibility issue with storybook, disabling for now
+// import vueDevTools from 'vite-plugin-vue-devtools' // compatibility issue with storybook https://github.com/vuejs/devtools/issues/703, disabling for now
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
