afe866a1644603145cbd5d3f0592b89d358bfb9e
muzafarova <alisapsee@gmail.com>
2025-09-18 11:55:35 +0100

    fix: use AppModalWithToggle component to simplify ProcudtCreate and ProductUpdate

--- src/pages/inventory/ProductCreate.vue
+++ src/pages/inventory/ProductCreate.vue
@@ -1,26 +1,26 @@
 <template>
-  <button
-    class="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white"
-    @click.stop="modalOpen = true"
-    aria-controls="add-product"
+  <AppModalWithToggle
+    modal-id="add-product"
+    modal-title="New product"
+    toggle-title="Add item"
+    toggle-variant="accent"
+    v-slot="slotProps"
   >
-    <svg class="fill-current shrink-0 sm:hidden" width="16" height="16" viewBox="0 0 16 16">
-      <path
-        d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z"
-      />
-    </svg>
-    <span class="max-sm:sr-only">Add item</span>
-  </button>
-
-  <AppModal id="add-product" :modal-open="modalOpen" title="New product" @close="modalOpen = false">
-    <div class="p-5">
-      <InventoryForm
-        v-model="newProduct"
-        :submit-label="inventoryStore.adding ? 'Submitting...' : 'Submit'"
-        @submit="handleCreate"
-      />
-    </div>
-  </AppModal>
+    <InventoryForm
+      v-model="newProduct"
+      :fields="fields"
+      :manufacturers="manufacturers"
+      :categories="categories"
+      :submit-label="inventoryStore.adding ? 'Submitting...' : 'Submit'"
+      @submit="
+        async () => {
+          await inventoryStore.addProduct(newProduct)
+          slotProps.close()
+          newProduct = DEFAULT
+        }
+      "
+    />
+  </AppModalWithToggle>
 </template>
 
 <script setup lang="ts">
@@ -28,22 +28,23 @@ import { ref } from 'vue'
 
 import { useInventoryStore } from '@/stores/inventory'
 
-import AppModal from '@/components/AppModal.vue'
+import AppModalWithToggle from '@/components/AppModalWithToggle.vue'
 import InventoryForm from './InventoryForm.vue'
 
+defineProps<{
+  fields: string[]
+  manufacturers: string[]
+  categories: string[]
+}>()
+
 const inventoryStore = useInventoryStore()
-const modalOpen = ref(false)
-const newProduct = ref({
+const DEFAULT = {
   name: '',
   manufacturer: '',
   category: '',
   quantity: 10,
   price: '',
   expiresAt: null,
-})
-
-async function handleCreate() {
-  await inventoryStore.addProduct(newProduct.value)
-  modalOpen.value = false
 }
+const newProduct = ref(DEFAULT)
 </script>
