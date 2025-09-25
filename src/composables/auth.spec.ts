2b5bb13db039daeee6ee91ef1e036da9dc1e719f
muzafarova <alisapsee@gmail.com>
2025-09-26 00:05:47 +0100

    refactor: useAuth composable

--- src/composables/auth.spec.ts
+++ src/composables/auth.spec.ts
@@ -1,38 +1,25 @@
 import { setActivePinia, createPinia } from 'pinia'
-import { describe, beforeEach, expect, it, vi } from 'vitest'
+import { describe, beforeEach, it, vi } from 'vitest'
 import { useAuth } from './auth'
 import { useAuthStore } from '@/stores/auth'
-// import { createApp } from 'vue'
 
 describe('auth', () => {
-  // let authStore: ReturnType<typeof useAuthStore>
-  // let app: any
-
   beforeEach(() => {
-    // creates a fresh pinia and makes it active
-    // so it's automatically picked up by any useStore() call
-    // without having to pass it to it: `useStore(pinia)`
     setActivePinia(createPinia())
-
-    // app = createApp({})
-    // const pinia = createPinia()
-    // app.use(pinia)
-    // setActivePinia(pinia)
-    // authStore = useAuthStore()
   })
 
-  it('should throw an error if hospitalId is not available', () => {
+  it('should throw an error if hospitalId is not available', ({ expect }) => {
     const authStore = useAuthStore()
 
     expect(authStore.getHospitalId()).toBeUndefined()
     expect(() => useAuth(() => Promise.resolve(null))).toThrow('Hospital ID is required')
   })
 
-  it('should augument the callback with the hospitalId as first parameter', () => {
+  it('should augument the callback with the hospitalId as first parameter', ({ expect }) => {
     const authStore = useAuthStore()
 
     vi.spyOn(authStore, 'getHospitalId').mockReturnValue('hospitalId')
-    const callback = vi.fn((x: string) => Promise.resolve(x))
+    const callback = vi.fn((...args: string[]) => Promise.resolve(args))
 
     useAuth(callback, '456')
 
--- src/composables/auth.ts
+++ src/composables/auth.ts
@@ -1,6 +1,9 @@
 import { useAuthStore } from '@/stores/auth'
 
-export function useAuth<T>(cb: (...args: unknown[]) => Promise<T>, ...args: unknown[]) {
+export function useAuth<T, Args extends unknown[]>(
+  cb: (hospitalId: string, ...args: Args) => Promise<T>,
+  ...args: Args
+) {
   const authStore = useAuthStore()
   const hospitalId = authStore.getHospitalId()
   if (!hospitalId) {
